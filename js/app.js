078a1dfbe000876ce4f6bb5521feaeab6b2c3342
Dani Llano <danillano97@gmail.com>
2023-09-14 22:32:31 -0300

    show timer

--- js/app.js
+++ js/app.js
@@ -6,31 +6,23 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
     document.querySelector('meta[name="theme-color"').setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff')
 })
 
+const start_screen = document.querySelector('#start-screen');
+const game_screen = document.querySelector('#game-screen');
+
 const cells = document.querySelectorAll('.main-grid-cell');
 
 const name_input = document.querySelector('#input-name');
-const start_screen = document.querySelector('#start-screen');
+
+const player_name = document.querySelector('#player-name');
+const game_level = document.querySelector('#game-level');
+const game_time = document.querySelector('#game-time')
 
 let level_index = 0;
 let level = CONSTANT.LEVEL[level_index];
 
-document.querySelector('#btn-level').addEventListener('click', (e) => {
-    level_index = level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
-    level = CONSTANT.LEVEL[level_index];
-    e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
-})
-
-document.querySelector('#btn-play').addEventListener('click', () => {
-    if (name_input.value.length > 0) {
-        alert(`level => ${level}`)
-    } else {
-        name_input.classList.add('input-err');
-        setTimeout(() => {
-            name_input.classList.remove('input-err');
-            name_input.focus()
-        }, 500);
-    }
-})
+let timer = null;
+let pause = false;
+let seconds = 0;
 
 const getGameInfo = () => JSON.parse(localStorage.getItem('game'));
 
@@ -46,6 +38,49 @@ const initGameGrid = () => {
         index++;
     }
 }
+
+const setPlayerName = (name) => localStorage.setItem('player_name', name);
+const getPlayerName = () => localStorage.getItem('player_name')
+
+const showTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
+
+const startGame = () => {
+    start_screen.classList.remove('active')
+    game_screen.classList.add('active')
+
+    player_name.innerHTML = name_input.value.trim();
+    setPlayerName(name_input.value.trim());
+
+    game_level.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+
+    seconds = 0;
+
+    timer = setInterval(() => {
+        if (!pause) {
+            seconds = seconds + 1;
+            game_time.innerHTML = showTime(seconds);
+            console.log(seconds)
+        }
+    }, 1000)
+}
+
+document.querySelector('#btn-level').addEventListener('click', (e) => {
+    level_index = level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
+    level = CONSTANT.LEVEL[level_index];
+    e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+})
+
+document.querySelector('#btn-play').addEventListener('click', () => {
+    if (name_input.value.trim().length > 0) {
+        startGame()
+    } else {
+        name_input.classList.add('input-err');
+        setTimeout(() => {
+            name_input.classList.remove('input-err');
+            name_input.focus()
+        }, 500);
+    }
+})
 
 const init = () => {
     const darkmode = JSON.parse(localStorage.getItem('darkmode'));
